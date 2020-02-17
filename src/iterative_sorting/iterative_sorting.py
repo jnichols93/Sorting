27ca9698629e01de0bbf117b76e6883b18a87661
Justin Nichols <51006450+jnichols93@users.noreply.github.com>
2020-02-17 15:21:29 -0600

    iterative sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -6,11 +6,14 @@ def selection_sort( arr ):
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-             
-
+        for j in range (cur_index, len(arr)): #comparision loop: starts as i +1 (index of range) and ends at length of array 
+            if arr[smallest_index] > arr[j]:
+                smallest_index = j
 
 
         # TO-DO: swap
+        if arr[smallest_index] > arr[j]:
+                smallest_index = j
 
 
 
