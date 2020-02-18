6296150d9daa417a44ec21fda3ac96e1dac52f24
Justin Nichols <51006450+jnichols93@users.noreply.github.com>
2020-02-18 10:29:34 -0600

    count-sort for warmup

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -32,6 +32,10 @@ def bubble_sort( arr ):
 
 
 # STRETCH: implement the Count Sort function below
+# Counting sort is a sorting algorithm that sorts the 
+# elements of an array by counting the number of occurrences of each unique element in the array. 
+# The count is stored in an auxiliary array and the sorting is done by mapping the count as an 
+# index of the auxiliary array.
 def count_sort( arr, maximum=-1 ):
 
     return arr
